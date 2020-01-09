e757fb51e5b5a7c3f57626cf218686ecb23e8018
chongchen <chenchong.blog@gmail.com>
2020-01-08 22:20:32 -0500

    Mac

---
+++ _06_AVLTree/src/ca/monor/Main.java
@@ -0,0 +1,11 @@
+package ca.monor;
+
+import ca.monor.tree.AVLTree;
+
+public class Main {
+    public static void main(String[] args) {
+        AVLTree<Integer> tree = new AVLTree<>();
+        tree.add(1);
+        tree.add(2);
+    }
+}
