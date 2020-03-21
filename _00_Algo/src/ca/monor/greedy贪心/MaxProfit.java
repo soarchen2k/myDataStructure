1ee971bf8fe42fc9bc8737c6df2cfec98531d6ac
soarchen <chenchong.blog@gmail.com>
2020-03-21 12:08:21 -0400

    iMac

--- _00_Algo/src/ca/monor/greedy贪心/MaxProfit.java
+++ _00_Algo/src/ca/monor/greedy贪心/MaxProfit.java
@@ -1,5 +1,6 @@
 package ca.monor.greedy贪心;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -8,16 +9,16 @@ import java.util.PriorityQueue;
  */
 public class MaxProfit {
     private static class Node {
-        public int profits;
-        public int costs;
+        public int profit;
+        public int cost;
 
         public Node(int p, int c) {
-            this.profits = p;
-            this.costs = c;
+            this.profit = p;
+            this.cost = c;
         }
     }
 
-    public static int findBestProject(int[] profits, int[] costs, int startMoney, int k) {
+    public static int findBestProject(int[] profits, int[] costs, int myMoney, int k) {
         Node[] nodes = new Node[profits.length];
         for (int i = 0; i < nodes.length; i++) {
             nodes[i] = new Node(profits[i], costs[i]);
@@ -31,20 +32,34 @@ public class MaxProfit {
         PriorityQueue<Node> maxProfilePriorityQueue =
                 new PriorityQueue<>(new MaxProfitComparator());
 
+        // 把所有项目加入小根堆
+        minCostPriorityQueue.addAll(Arrays.asList(nodes));
 
+        for (int i = 0; i < k; i++) {
+            while (!minCostPriorityQueue.isEmpty()
+                    && minCostPriorityQueue.peek().cost <= myMoney) {
+                maxProfilePriorityQueue.add(minCostPriorityQueue.poll());
+            }
+            // 资金回笼
+            if (maxProfilePriorityQueue.isEmpty()) {
+                return myMoney;
+            }
+            myMoney += maxProfilePriorityQueue.poll().profit;
+        }
+        return myMoney;
     }
 
     private static class MinCostComparator implements Comparator<Node> {
         @Override
         public int compare(Node o1, Node o2) {
-            return o1.costs - o2.costs;
+            return o1.cost - o2.cost;
         }
     }
 
     private static class MaxProfitComparator implements Comparator<Node> {
         @Override
         public int compare(Node o1, Node o2) {
-            return o2.profits - o1.profits;
+            return o2.profit - o1.profit;
         }
     }
 }
